9404417a32c7be652804fcfae225fab0db74d554
LAM <chinghei1007@gmail.com>
2023-11-29 21:10:05 +0800

    2911.2109

--- src/ShowTable.java
+++ src/ShowTable.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ShowTable {
-    public static void showtable(Statement statement) throws SQLException {
-        ResultSet resultSet = statement.executeQuery("select * from products");
+    public static void showtable(Statement statement, String sql) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(sql);
         ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
         int colCount=resultSetMetaData.getColumnCount();
         System.out.println(colCount);
@@ -18,13 +21,55 @@ public class ShowTable {
             tableModel.addColumn(resultSetMetaData.getColumnName(i));
             colNames[i-1] = resultSetMetaData.getColumnName(i);
         }
+
+        while (resultSet.next()) {
+            Object[] row = new Object[colCount];
+            for (int i = 1; i <= colCount; i++) {
+                row[i - 1] = resultSet.getObject(i);
+            }
+            tableModel.addRow(row);
+        }
+
         JTable table = new JTable(tableModel);
         JScrollPane jScrollPane = new JScrollPane(table);
 
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Test");
-        jFrame.setSize(500,600);
+        jFrame.setSize(900,600);
+        jFrame.setLocationRelativeTo(null);
         jFrame.add(jScrollPane);
+        //Column width auto
+        int cellPadding = 10;
+        int headerPadding = 10;
+
+        // Resize columns based on maximum content length and column headers
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            TableColumn column = table.getColumnModel().getColumn(i);
+            TableCellRenderer headerRenderer = column.getHeaderRenderer();
+            if (headerRenderer == null) {
+                headerRenderer = table.getTableHeader().getDefaultRenderer();
+            }
+            Component headerComp = headerRenderer.getTableCellRendererComponent(table, column.getHeaderValue(), false, false, 0, 0);
+            int headerWidth = headerComp.getPreferredSize().width + headerPadding;
+
+            int preferredWidth = Math.max(headerWidth, column.getMinWidth());
+            int maxWidth = column.getMaxWidth();
+
+            for (int row = 0; row < table.getRowCount(); row++) {
+                TableCellRenderer cellRenderer = table.getCellRenderer(row, i);
+                Component cellComp = table.prepareRenderer(cellRenderer, row, i);
+                int cellWidth = cellComp.getPreferredSize().width + cellPadding;
+                preferredWidth = Math.max(preferredWidth, cellWidth);
+                if (preferredWidth >= maxWidth) {
+                    preferredWidth = maxWidth;
+                    break;
+                }
+            }
+
+            column.setPreferredWidth(preferredWidth);
+        }
+
         jFrame.setVisible(true);
 
     }
--- src/creatingfata.java
+++ src/creatingfata.java
@@ -2,11 +2,17 @@ import org.h2.Driver;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class creatingfata {
     //public static void dataInsert() throws SQLException {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
 
         Connection connection;
         String username = "test";
@@ -15,8 +21,27 @@ public class creatingfata {
         DriverManager.registerDriver(new org.h2.Driver());
         connection = DriverManager.getConnection(url,username,password);
         Statement statement = connection.createStatement();
-        //statement.executeUpdate("create table products (id INT, category VARCHAR(20), price Decimal(12),brand VARCHAR(50), name VARCHAR(70))");
-        ResultSet resultSet = statement.executeQuery("select * from products");
+
+        //製造table
+        //statement.executeUpdate("create table products (id INT, name VARCHAR(256), category VARCHAR(20), price Decimal(12),brand VARCHAR(50) )");
+        //statement.executeUpdate("insert into products1 select id,name,category,price,brand from products");
+        //statement.executeUpdate("DROP TABLE products;");
+
+
+        //添加record1
+        /*BufferedReader reader = new BufferedReader(new FileReader("records1.txt"));
+        List<String> satement = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null){
+            if (line.isEmpty()){continue;}
+            satement.add(line);
+        }
+        for (String string : satement) {
+            statement.executeUpdate(string);
+        }
+*/
+        String sql = "select * from products";
+        ShowTable.showtable(statement,sql);
         /*
         ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
         int colCount=resultSetMetaData.getColumnCount();
